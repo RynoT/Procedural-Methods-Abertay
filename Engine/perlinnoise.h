5c006b16763ae790880b9fbbef14ed896d34bd37
Ryan Thomson <1502306@abertay.ac.uk>
2017-04-06 22:15:36 +0100

    Initial Commit

---
+++ Engine/perlinnoise.h
@@ -0,0 +1,26 @@
+#pragma once
+
+struct Permutation
+{
+	Permutation(const int *permArray);
+
+	int value[512];
+};
+
+class PerlinNoise
+{
+	PerlinNoise() { }
+
+public:
+	static double noise(double x, double y, double z);
+
+private:
+	inline static double fade(const double& t);
+
+	inline static double lerp(const double& t, const double& a, const double& b);
+
+	static double grad(const int& hash, const double& x, const double& y, const double& z);
+
+	static const int m_PermArray[];
+	static const Permutation m_Permutation;
+};
